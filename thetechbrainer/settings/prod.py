070186965d6604634792898aadb894a3784bb577
satyasashi <sashikanthpeddinti@gmail.com>
2020-11-02 14:46:17 +0000

    changes from prod

--- thetechbrainer/settings/prod.py
+++ thetechbrainer/settings/prod.py
@@ -3,7 +3,7 @@ from thetechbrainer.settings.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS += ['www.thetechbrainer.com', '206.189.129.28', 'localhost', 'thetechbrainer.com']
+ALLOWED_HOSTS += ['www.thetechbrainer.com', '143.110.183.187', 'thetechbrainer.com']
 
 DATABASES = {
     'default': get_secret_key("PROD_DATABASE"),
