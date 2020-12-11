d35dace742ad1564be4710243321a34a9d62fff9
satyasashi <sashikanthpeddinti@gmail.com>
2020-12-11 06:46:12 +0000

    prod changes

--- thetechbrainer/settings/prod.py
+++ thetechbrainer/settings/prod.py
@@ -1,7 +1,7 @@
 from thetechbrainer.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS += ['www.thetechbrainer.com', '143.110.183.187', 'thetechbrainer.com']
 
